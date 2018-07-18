79f25607f3c08f2151b17add40494a38f40361ca
Carlos Martinez <itscarlosisaac@gmail.com>
2018-07-18 14:06:44 -0400

    Added events components.

--- client/components/Event.js
+++ client/components/Event.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-import Event from './Event';
+import PropTypes from 'prop-types';
 
 export default class Event extends Component {
   render() {
     return (
-      <div className="app__headline">
-        <h1>Upcoming Events</h1>
-        <Event />
+      <div>
+        <img src={this.props.data.thumbnail} width="200" />
+        <h1>{this.props.data.title}</h1>
+        <p>{this.props.data.description}</p>
       </div>
     )
   }
+}
+
+Event.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  excerpt: PropTypes.string.isRequired,
+  leader: PropTypes.string.isRequired,
+  date: PropTypes.any.isRequired,
+  location: PropTypes.string.isRequired,
+  speakers: PropTypes.arrayOf(PropTypes.string),
 }
--- client/components/EventList.js
+++ client/components/EventList.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import Event from './Event';
+import Data from '../dummy/data';
 
 export default class EventList extends Component {
+
+  state = {
+    events: []
+  }
+
+  componentWillMount = () => {
+    this.setState({ events: Data} )
+  }
+  
   render() {
     return (
-      <div>
-        EventList
-      </div>
+      <Fragment>
+        <div className="app__event__list">
+          <header className="app__title">
+            <h1>Upcoming Events</h1>
+            <p>Latest added events</p>
+          </header>
+          <div className="app__events">
+          {
+            this.state.events.map( (event) => {
+              return <Event data={event} />
+            }) 
+          }
+          </div>
+        </div>
+      </Fragment>
     )
   }
 }
